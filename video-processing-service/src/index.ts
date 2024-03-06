dc3034def676b1532aa2f78e7a09473d2c5f10f3
Blend Dehari <blenddehari@gmail.com>
2024-03-06 09:10:38 +0100

    process video locally
    * Resize the video to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express()
-const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/process-video', (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath
+    const outputFilePath = req.body.outputFilePath
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send('Bad request. Missing input or output file path')
+    }
+
+    // Process the video
+    ffmpeg(inputFilePath)
+    // Resize the video to 360p
+        // .outputOptions(['-vf', 'scale=-1:360']) //this does not work for me so I used the below
+        .outputOptions('-vf', 'scale=640:360')
+        .on('end', () => {
+            console.log('Video processing finished')
+            res.status(200).send('Video processing finished successfully')
+        })
+        .on('error', (err) => {
+            console.log('Error processing video:', err)
+            res.status(500).send('Error processing video')
+        })
+        .save(outputFilePath)
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`)
 })
