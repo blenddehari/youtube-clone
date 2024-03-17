4defef3f55e6083c3f449257ed8f6dff44766365
Blend Dehari <blenddehari@gmail.com>
2024-03-17 13:08:27 +0100

    initialize the next js web client

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadVideoFromBucket, setupDirectories, uploadVideoToBucket } from './storage';
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadVideoFromBucket, setupDirectories, uploadVideoToBucket } from './storage'; 
 
 setupDirectories();
 
@@ -8,7 +8,7 @@ app.use(express.json())
 
 // this endpoint will be invoked by the cloud pub/sub message, not a user
 app.post('/process-video', async (req, res) => {
-    //    Get the bucket and filename from the Clound Pub/Sub message
+    //    Get the bucket and filename from the Cloud Pub/Sub message
     let data;
     try {
         const message = Buffer.from(req.body.message.data, 'base64').toString('utf-8');
