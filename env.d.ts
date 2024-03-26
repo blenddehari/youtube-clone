4b3c2aae94fa3a7793dfdb4326092a7e7df9358a
Blend Dehari <blenddehari@gmail.com>
2024-03-26 22:34:53 +0100

    implement an event-driven function that stores newly authenticated users in firestore

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    FIREBASE_API_KEY: string;
+    // define other environment variables as needed
+  }
+}
--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -7,7 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
@@ -17,3 +19,21 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+initializeApp()
+
+const firestore = new Firestore()
+
+// event-driven function/architecture - when a user is created, create a user document in the firestore
+export const createUser = functions.auth.user().onCreate((user) => {
+	const userInfo = {
+		uid: user.uid,
+		email: user.email,
+		photoUrl: user.photoURL,
+	}
+
+	// if the collection or doc does not exist it will be created automatically (upsert)
+	firestore.collection("users").doc(user.uid).set(userInfo)
+	logger.info(`User created: ${JSON.stringify(userInfo)}`)
+	return
+});
