d56c65f715a1f49654a2e2a4c05d75636ec0a15e
Blend Dehari <blenddehari@gmail.com>
2024-03-26 22:47:44 +0100

    use env variables for all firebase config data
    Note: for this to work using Next.js, all environment variables must be prefixed with NEXT_PUBLIC_

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
@@ -14,16 +16,15 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "blend-yt-clone.firebaseapp.com",
-  projectId: "blend-yt-clone",
-  appId: "1:347929531734:web:db7ba25c7c8112050c72f7",
-  measurementId: "G-MNMNB0284E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 
 // const analytics = getAnalytics(app);
