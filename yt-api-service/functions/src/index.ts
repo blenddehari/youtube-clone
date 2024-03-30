6da276254e5c7332598ae9c314ae0d29d0f5ff05
Blend Dehari <blenddehari@gmail.com>
2024-03-30 22:02:50 +0100

    create and deploy new function to generate the upload url (signed url)
    + update cors policy on the blend-yt-raw-videos bucket with the following command (executed in root):
    gcloud storage buckets update gs://blend-yt-raw-videos --cors-file=utils/gcs-cors.json

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -11,6 +11,8 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -23,6 +25,9 @@ import * as logger from "firebase-functions/logger";
 initializeApp()
 
 const firestore = new Firestore()
+const storage = new Storage()
+
+const rawVideoBucketName = 'blend-yt-raw-videos';
 
 // event-driven function/architecture - when a user is created, create a user document in the firestore
 export const createUser = functions.auth.user().onCreate((user) => {
@@ -37,3 +42,26 @@ export const createUser = functions.auth.user().onCreate((user) => {
 	logger.info(`User created: ${JSON.stringify(userInfo)}`)
 	return
 });
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+	// Check if the user is authenticated
+	if (!request.auth) {
+		throw new functions.https.HttpsError("failed-precondition", "The function must be called while authenticated.")
+	}
+
+	const auth = request.auth
+	const data = request.data
+	const bucket = storage.bucket(rawVideoBucketName)
+
+	// Generate a unique filename
+	const fileName = `${auth.uid}-${Date.now()}-${data.filename}.${data.fileExtension}`
+
+	// Get a v4 signed URL for uploading the video
+	const [url] = await bucket.file(data.filename).getSignedUrl({
+		version: 'v4',
+		action: 'write',
+		expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+	})
+
+	return {url, fileName}
+})
